95b826c672cd25fa0a96a6741daa43ed273e8bd7
lkelvinsilva <lkelvinsilva@gmail.com>
2025-11-13 17:42:10 -0300

    Corrige rota do webhook

---
+++ api/webhook.js
@@ -0,0 +1,19 @@
+export default function handler(req, res) {
+  if (req.method === 'GET') {
+    const mode = req.query['hub.mode'];
+    const token = req.query['hub.verify_token'];
+    const challenge = req.query['hub.challenge'];
+
+    if (mode && token === process.env.VERIFY_TOKEN) {
+      console.log("Webhook verificado!");
+      res.status(200).send(challenge);
+    } else {
+      res.sendStatus(403);
+    }
+  } else if (req.method === 'POST') {
+    console.log("Mensagem recebida:", req.body);
+    res.sendStatus(200);
+  } else {
+    res.sendStatus(404);
+  }
+}
