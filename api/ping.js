358a783122ea8b404185e55a6731e6cbb3b7e62f
lkelvinsilva <lkelvinsilva@gmail.com>
2025-11-14 16:04:35 -0300

    Add ping endpoint

---
+++ api/ping.js
@@ -0,0 +1,9 @@
+export default function handler(req, res) {
+  const token = req.query.token;
+
+  if (token !== process.env.PING_TOKEN) {
+    return res.status(403).json({ error: "invalid token" });
+  }
+
+  return res.status(200).json({ ok: true });
+}
