5743f6f4ae1a21761af74cb59c45c2ea95dc31d2
lkelvinsilva <lkelvinsilva@gmail.com>
2025-11-14 17:13:36 -0300

    Fix ping route

--- api/ping.js
+++ api/ping.js
@@ -1,11 +1,13 @@
 export default function handler(req, res) {
   const token = req.query.token;
 
+  if (!token) {
+    return res.status(400).json({ error: "token missing" });
+  }
+
   if (token !== process.env.PING_TOKEN) {
     return res.status(403).json({ error: "invalid token" });
   }
 
   return res.status(200).json({ ok: true });
 }
-
-console.log("TOKEN SALVO NA VERCEL:", process.env.PING_TOKEN);
