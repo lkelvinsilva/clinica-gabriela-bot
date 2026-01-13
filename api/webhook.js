5af6d97d5ce986feb91729daddd8c74a1b46cf67
lkelvinsilva <lkelvinsilva@gmail.com>
2026-01-13 20:14:16 -0300

    Resolve conflitos de merge e mantém correções do fluxo WhatsApp

--- api/webhook.js
+++ api/webhook.js
@@ -2,7 +2,6 @@ import axios from "axios";
 import { getUserState, setUserState, isDuplicateMessage } from "../utils/state.js";
 import { isTimeSlotFree, createEvent, getAvailableSlots } from "../utils/googleCalendar.js";
 import { isWithinBusinessHours } from "../utils/googleCalendar.js";
-import { isWithinBusinessHours } from "../utils/googleCalendar.js";
 import { appendRow } from "../utils/googleSheets.js";
 import { notifyAdminNewAppointment,sendConfirmationTemplate } from "../utils/whatsapp.js";
 
